1a411096fc5133d3aa08db798947f8088f11fc45
Admin <Admin@DESKTOP-JE45FR8>
2020-03-22 12:28:40 +0100

    delete override annotation

--- UHPlatformer/src/com/golding/platformer/gameStates/LevelOneState.java
+++ UHPlatformer/src/com/golding/platformer/gameStates/LevelOneState.java
@@ -10,11 +10,11 @@ public class LevelOneState extends GameState
     private Player player;
     private Map map;
     
-    public LevelOneState(final GameStateManager gsm) {
+    public LevelOneState(GameStateManager gsm) {
         super(gsm);
     }
     
-    @Override
+    
     public void init() {
         this.player = new Player(30, 30);
         this.map = new Map("/map1.UHP");
@@ -22,24 +22,24 @@ public class LevelOneState extends GameState
         LevelOneState.yOffset = -600.0;
     }
     
-    @Override
+    
     public void tick() {
         this.player.tick(this.map.getBlocks());
     }
     
-    @Override
-    public void draw(final Graphics g) {
+    
+    public void draw(Graphics g) {
         this.player.draw(g);
         this.map.draw(g);
     }
     
-    @Override
-    public void keyPressed(final int k) {
+    
+    public void keyPressed(int k) {
         this.player.keyPressed(k);
     }
     
-    @Override
-    public void keyReleased(final int k) {
+    
+    public void keyReleased(int k) {
         this.player.keyReleased(k);
     }
 }
